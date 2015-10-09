1a66dfa44abe7fa5e96cbc6f5dd7a38b303a397f
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-10-09 13:48:32 -0700

    documentation

--- src/TagsSelector.js
+++ src/TagsSelector.js
@@ -5,11 +5,21 @@ import core from 'bower:metal/src/core';
 import SoyComponent from 'bower:metal/src/soy/SoyComponent';
 import './TagsSelector.soy';
 
+/**
+ * TagsSelector component.
+ */
 class TagsSelector extends SoyComponent {
+	/**
+	 * @inheritDoc
+	 */
 	constructor(opt_config) {
 		super(opt_config);
 	}
 
+	/**
+	 * Adds a tag to the tags list.
+	 * @param {!string} tag
+	 */
 	addTag(tag) {
 		if (!this.hasTag_(tag)) {
 			this.tags = this.tags.concat(tag);
@@ -21,6 +31,11 @@ class TagsSelector extends SoyComponent {
 		}
 	}
 
+	/**
+	 * Focuses the tag input.
+	 * @param {!string} id The id of the component.
+	 * @private
+	 */
 	focusInput_(id) {
 		var input = document.getElementById(id + '-input');
 
@@ -29,6 +44,11 @@ class TagsSelector extends SoyComponent {
 		}
 	}
 
+	/**
+	 * Handles the focusout event from the tags input.
+	 * @param {!Event} event
+	 * @protected
+	 */
 	handleFocusout_(event) {
 		var inputElement = event.target;
 		var value = inputElement.value;
@@ -42,6 +62,11 @@ class TagsSelector extends SoyComponent {
 		}
 	}
 
+	/**
+	 * Handles the keypress event from the tags input.
+	 * @param {!Event} event
+	 * @protected
+	 */
 	handleKeypress_(event) {
 		var char = event.keyCode;
 
@@ -65,6 +90,11 @@ class TagsSelector extends SoyComponent {
 		}
 	}
 
+	/**
+	 * Handles click event triggered from the remove button.
+	 * @param {!Event} event
+	 * @protected
+	 */
 	handleRemoveClick_(event) {
 		var tagElement = event.target.parentElement;
 		var tag = tagElement.textContent;
@@ -72,18 +102,37 @@ class TagsSelector extends SoyComponent {
 		this.removeTag(tag);
 	}
 
+	/**
+	 * Checks if tag already exists.
+	 * @param {!string} tag
+	 * @return {boolean}
+	 * @protected
+	 */
 	hasTag_(tag) {
 		return this.tags.indexOf(tag) !== -1;
 	}
 
+	/**
+	 * Removes all tags.
+	 */
 	removeAll() {
 		this.tags = [];
 	}
 
+	/**
+	 * Removes delimeter and trims whitespace of next tag.
+	 * @param {!string} value
+	 * @return {string} value with delimeter removed.
+	 * @protected
+	 */
 	removeDelim_(value) {
 		return value.replace(this.delimeter, '').trim();
 	}
 
+	/**
+	 * Removes tag from the list.
+	 * @param {!string} tag
+	 */
 	removeTag(tag) {
 		if (this.hasTag_(tag)) {
 			var index = this.tags.indexOf(tag);
@@ -99,12 +148,41 @@ class TagsSelector extends SoyComponent {
 		}
 	}
 
+	/**
+	 * Gets the tags currently selected.
+	 * @return {Array<string>}
+	 */
 	val() {
 		return this.tags;
 	}
 }
 
+/**
+ * Default tooltip elementClasses.
+ * @default tooltip
+ * @type {string}
+ * @static
+ */
+TagsSelector.ELEMENT_CLASSES = 'tagsSelector';
+
+/**
+ * KeyCode for the Enter key.
+ * @default 13
+ * @type {int}
+ * @static
+ */
+TagsSelector.ENTER_KEYCODE = 13;
+
+/**
+ * TagsSelector attrbutes definition.
+ * @type {!Object}
+ * @static
+ */
 TagsSelector.ATTRS = {
+	/**
+	 * Character to separate tags.
+	 * @type {!string}
+	 */
 	delimeter: {
 		validator: function(val) {
 			return core.isString && val.length === 1;
@@ -112,20 +190,26 @@ TagsSelector.ATTRS = {
 		value: ','
 	},
 
+	/**
+	 * Text that is the placeholder in the tags input.
+	 * @type {string}
+	 * @default ''
+	 */
 	placeholder: {
 		validator: core.isString,
 		value: ''
 	},
 
+	/**
+	 * Text that is the placeholder in the tags input.
+	 * @type {!Array<string>}
+	 * @default []
+	 */
 	tags: {
 		validator: core.isArray,
 		value: []
 	}
 };
-
-TagsSelector.ELEMENT_CLASSES = 'tagsSelector';
-
-TagsSelector.ENTER_KEYCODE = 13;
 
 ComponentRegistry.register('TagsSelector', TagsSelector);
 
